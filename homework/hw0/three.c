fd2d22dfa39051cd5e2ad6d82099974386a881c5
Edgar Robitaille <erobita1@jh.edu>
2023-02-03 18:48:21 -0500

    Finalized Homework 0

--- homework/hw0/three.c
+++ homework/hw0/three.c
@@ -1,11 +1,8 @@
-//Edgar Robitaille                                                                                                                                                                                         
-//erobita1                                                                                                                                                                                                
-                                                                                                                                                                                                           
+//Edgar Robitaille
+//erobita1
+
 #include <stdio.h>
 int main(void) {
    printf("The third prize goes to Pat.\n");
    return 0;
 }
-
-
-
--- homework/hw0/two.c
+++ homework/hw0/two.c
@@ -1,9 +1,6 @@
-//Edgar Robitaille                                                                                                                                                                                         
-//erobita1                                                                                                                                                                                                  
+//Edgar Robitaille                                                                                                                                                               //erobita1                                                                                                                                                                                                  
 #include <stdio.h>
 int main(void) {
    printf("The second prize goes to Gongqi.\n");
    return 0;
 }
-
-
