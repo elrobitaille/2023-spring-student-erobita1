342f004757a1e42f7976bd04ec68d321c57f1d81
Edgar Robitaille <erobita1@jh.edu>
2023-02-22 18:38:06 -0500

    Now calls match function in main

--- homework/hw3/hw3.c
+++ homework/hw3/hw3.c
@@ -16,25 +16,34 @@ int main(int argc, const char *argv[]) {
   /* Check if there is a correct number of arguments when running the program, 2 or 3 depending if there is a restriction.*/
   if (argc != 2 && argc != 3) {
     fprintf(stderr, "Wrong number of arguments\n");
-    return 1;
+    return -1;
   }
 
   /* Read the file and check if the file has a valid path or address. If not, catch error. */
   FILE *input_file = fopen(argv[1], "r");
   if (input_file == NULL) {
     fprintf(stderr, "Error opening file\n");
-    return 1;
+    return -1;
   }
 
   int num_words = read_file(input_file, words, MAX_WORD_NUM); //Check if there is an issue in functions.c.
 
-  if (num_words != 0) {
+  /* If the number of words is zero or less, there is an issue in functions.c. */
+  if (num_words <= 0) {
     fprintf(stderr, "Error in functions.c");
+    return -1;
   }
 
   int restriction_length = DEFAULT_RESTRICTION; 
   if (argc == 3) {
     restriction_length = atoi(argv[2]); // Set restriction length from the command line argument, utilize atoi from stdlib.h.
+  }
+
+  /* Call the match function to see if matches regex pattern. */
+  for (int i = 0; i < num_words; i++) {
+    if (match(argv[1], words[i], restriction_length)) {
+      printf("%s\n", words[i]);
+    }
   }
 
   fclose(input_file); //Close the input file.
