cabc1c845a711729f13cd025d618527f2e749a8a
Edgar Robitaille <erobita1@jh.edu>
2023-03-24 17:01:54 -0400

    Added header guards and basic .h setup

--- homework/hw5/language_model.h
+++ homework/hw5/language_model.h
@@ -1,2 +1,12 @@
 // Edgar Robitaille
 // erobita1
+
+#ifndef LANGUAGE_MODEL_H
+#define LANGUAGE_MODEL_H
+
+int handle_a_command(void);
+int handle_d_command(void);
+int handle_c_command(void);
+int handle_f_command(void);
+
+#endif 
