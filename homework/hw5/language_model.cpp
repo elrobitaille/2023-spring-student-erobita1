f30027ebdcfb045a84e22365484de14c8d8f303c
Edgar Robitaille <erobita1@jh.edu>
2023-03-23 23:40:34 -0400

    Adding command helper functions

--- homework/hw5/language_model.cpp
+++ homework/hw5/language_model.cpp
@@ -1,2 +1,28 @@
 // Edgar Robitaille
 // erobita1
+
+#include <iostream>
+#include "language_model.h"
+
+using std::cout;
+using std::endl;
+
+int handle_a_command(void) {
+    
+    return 0;
+}
+
+int handle_d_command(void) {
+    
+    return 0;
+}
+
+int handle_c_command(void) {
+    
+    return 0;
+}
+
+int handle_f_command(void) {
+    
+    return 0;
+}
