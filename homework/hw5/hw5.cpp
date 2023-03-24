a2c1291a8f10dcd3608f3ae63b74fa695e73a2d3
Edgar Robitaille <erobita1@jh.edu>
2023-03-23 23:34:41 -0400

    Added switch case for different commands

--- homework/hw5/hw5.cpp
+++ homework/hw5/hw5.cpp
@@ -6,7 +6,26 @@
 using std::cout;
 using std::endl;
 
-int main() {
-    cout << "Hello world!" << endl;
+int main(int argc, char *argv[]) {
+    // Check the second command line argument and do the corresponding action to the letter, similar to the midterm project.
+    switch (*argv[1]) {
+        case 'a':
+            //handle_a_command();
+            break;
+        case 'd':
+            //handle_d_command();
+            break;
+        case 'c':
+            //handle_c_command();
+            break;
+        case 'f':
+            //handle_f_command();
+            break;
+        default: 
+            cout << "Invalid command: valid options are a, d, c, and f" << endl;
+            break;
+
+    }
+    
     return 0;
 }
