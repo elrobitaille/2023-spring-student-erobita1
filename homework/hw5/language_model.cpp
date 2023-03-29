7b0d4f0e40efd30c5f819d6ac3876d6f0278ba87
Edgar Robitaille <erobita1@jh.edu>
2023-03-28 20:37:38 -0400

    Added hashmap

--- homework/hw5/language_model.cpp
+++ homework/hw5/language_model.cpp
@@ -4,21 +4,34 @@
 #include <iostream>
 #include <string> 
 #include <fstream>
+#include <unordered_map>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
 using std::ifstream;
+using std::unordered_map;
 
 int handle_a_command(ifstream& input_file) {
     if (!input_file.is_open()) {
         return 1;
     }
 
+    unordered_map<string, int> word_frequency;
+
     string word;
     while (input_file >> word) {
-      cout << word << endl;
+        ifstream inner_file(word);
+        if (!inner_file.is_open()) {
+            cout << "Invalid file: " << word << endl;
+        } 
+
+        while (inner_file >> word) {
+            word_frequency[word] += 1;
+        }
+
+        inner_file.close();
     }
     
     return 0;
