fd296abe47890b89087a9841976c4a04ec296ca8
Edgar Robitaille <erobita1@jh.edu>
2023-04-01 13:58:13 -0400

    Added detailed comments

--- homework/hw5/language_model.h
+++ homework/hw5/language_model.h
@@ -1,6 +1,9 @@
 // Edgar Robitaille
 // erobita1
 
+/* This header has the declarations for functions in language_model.cpp for the basic operations 
+of the trigram language model. */
+
 #include <iostream>
 #include <string> 
 #include <fstream>
@@ -8,15 +11,21 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
-#include <utility>
 
 #ifndef LANGUAGE_MODEL_H
 #define LANGUAGE_MODEL_H
 
+// Create a hashmap and find frequencies of the trigrams
 std::map<std::string, int> find_frequencies(std::ifstream& input_file);
+// Compare each trigram based on their frequency and alphabetical order
+bool compare_trigrams(const std::pair<std::string, int>& first_pair, const std::pair<std::string, int>& second_pair);
+// Handles 'a' to print all the trigrams in ascending order. 
 int handle_a_command(std::ifstream& input_file);
+// Handles 'd' to print all the trigrams in descending order. 
 int handle_d_command(std::ifstream& input_file);
+// Handles 'c' to print all the trigrams in descending numerical order. 
 int handle_c_command(std::ifstream& input_file);
+// Handles 'f' to search for a specific trigram. 
 int handle_f_command(std::ifstream& input_file, const std::string& first_word, const std::string& second_word);
 
 #endif 
