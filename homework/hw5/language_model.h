7f44c6863bc7acbdd2e78215ccf2926e8e5874d0
Edgar Robitaille <erobita1@jh.edu>
2023-04-01 03:46:13 -0400

    Updated function declarations

--- homework/hw5/language_model.h
+++ homework/hw5/language_model.h
@@ -1,15 +1,22 @@
 // Edgar Robitaille
 // erobita1
 
-#include <string>
-#include <unordered_map>
+#include <iostream>
+#include <string> 
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include <utility>
+
 #ifndef LANGUAGE_MODEL_H
 #define LANGUAGE_MODEL_H
 
-std::unordered_map<std::string, int> find_frequencies(std::ifstream& input_file);
+std::map<std::string, int> find_frequencies(std::ifstream& input_file);
 int handle_a_command(std::ifstream& input_file);
 int handle_d_command(std::ifstream& input_file);
 int handle_c_command(std::ifstream& input_file);
-int handle_f_command(std::ifstream& input_file);
+int handle_f_command(std::ifstream& input_file, const std::string& first_word, const std::string& second_word);
 
 #endif 
