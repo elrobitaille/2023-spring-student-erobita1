c2235531f4facb4b1bd8b39fb6c264643d9a561a
Edgar Robitaille <erobita1@jh.edu>
2023-04-01 18:58:43 -0400

    Header file for test file

---
+++ homework/hw5/model_tests.h
@@ -0,0 +1,23 @@
+// Edgar Robitaille
+// erobita1
+
+#include <iostream>
+#include <string> 
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <sstream>
+#include "language_model.h"
+#include <ios>
+
+#ifndef MODEL_TESTS_H
+#define MODEL_TESTS_H
+
+// Functions to be tested
+bool test_handle_f_command();
+bool test_find_frequencies();
+bool test_compare_trigrams();
+
+#endif 
