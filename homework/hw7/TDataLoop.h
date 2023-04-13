210d537cdd29c0fe6a61b20539266ec89d4b8efd
Edgar Robitaille <erobita1@jh.edu>
2023-04-13 10:15:26 -0400

    Basic layout of TDataLoop.h

---
+++ homework/hw7/TDataLoop.h
@@ -0,0 +1,17 @@
+// Edgar Robitaille
+// erobita1
+#ifndef TDATALOOP_H
+#define TDATALOOP_H
+
+template <typename DataType>
+class TDataLoop {
+public:
+
+private:
+
+};
+
+
+#include "TDataLoop.inc"
+
+#endif // TDATALOOP_H
