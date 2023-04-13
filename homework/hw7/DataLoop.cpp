6bda8176540887f1c7862d171f54b739f4a5d1b6
Edgar Robitaille <erobita1@jh.edu>
2023-04-13 10:05:00 -0400

    Added destructor for DataLoop

--- homework/hw7/DataLoop.cpp
+++ homework/hw7/DataLoop.cpp
@@ -1,6 +1,29 @@
+// Edgar Robitaille
+// erobita1
 #include <iostream>
 
 #include "DataLoop.h"
 
-DataLoop::DataLoop() : start(nullptr), count(0) { }
+DataLoop::DataLoop() : start(nullptr), count(0) {}
 
+DataLoop::DataLoop(const int &num) : count(1) {
+    start = new _Node({num, nullptr, nullptr});
+    start->next = start;
+    start->prev = start;
+}
+
+DataLoop::~DataLoop() {
+    if (count > 0) {
+        _Node* currentNode = start;
+        _Node* nextNode;
+
+        for (size_t i = 0; i < count; ++i) {
+            nextNode = currentNode->next;
+            delete currentNode;
+            currentNode = nextNode;
+        }
+
+        start = nullptr;
+        count = 0;
+    }
+}
