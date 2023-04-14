664b2a22fe58a61c0548a38546d4ffa3c53af88b
Edgar Robitaille <erobita1@jh.edu>
2023-04-14 19:16:03 -0400

    Adding operator= and comments updated

--- homework/hw7/DataLoop.cpp
+++ homework/hw7/DataLoop.cpp
@@ -15,17 +15,24 @@ DataLoop::DataLoop(const int &num) : count(1) {
     start->prev = start;
 }
 
+/* Create the copy constructor */
 DataLoop::DataLoop(const DataLoop & rhs) {
+    // If object is empty, create empty DataLoop object and simply return. */
     if (rhs.count == 0) {
         return;
     }
 
+    // Set next and prev pointers to itself after creating new node with null pointers. 
     start = new _Node{rhs.start->data, nullptr, nullptr};
     start->next = start;
     start->prev = start;
 
+    // Then, initialize the pointers. 
     _Node *current = rhs.start->next;
-    _Node *newNode, *prevNode = start;
+    _Node *newNode = nullptr;
+    _Node *prevNode = start;
+
+    // Iterate through the nodes and update accordingly 
     while (current != rhs.start) {
         newNode = new _Node{current->data, prevNode, nullptr};
         prevNode->next = newNode;
@@ -33,6 +40,8 @@ DataLoop::DataLoop(const DataLoop & rhs) {
         prevNode = newNode;
     }
 
+    /* Update for the last new node's pointer to new start node and new start 
+       prev pointer to last new node, which will complete the 'circular' list. */  
     prevNode->next = start;
     start->prev = prevNode;
 
@@ -57,7 +66,28 @@ DataLoop::~DataLoop() {
 }
 
 DataLoop & DataLoop::operator=(const DataLoop & rhs) {
-    
+    if (this == &rhs) {
+        return *this;
+    }
+
+    if (count > 0) {
+        _Node* currentNode = start;
+        _Node* nextNode;
+
+        for (size_t i = 0; i < count; ++i) {
+            nextNode = currentNode->next;
+            delete currentNode;
+            currentNode = nextNode;
+        }
+    }
+
+    count = rhs.count; 
+    if (rhs.count == 0) {
+        start = nullptr;
+        return *this;
+    }
+
+    return *this;
 }
 
 bool DataLoop::operator==(const DataLoop & rhs) const {
