bf2e7fe57abf2a5e0438ba3a5e21d9d008d99f2f
Edgar Robitaille <erobita1@jh.edu>
2023-04-14 17:24:59 -0400

    Added copy constructor

--- homework/hw7/DataLoop.cpp
+++ homework/hw7/DataLoop.cpp
@@ -4,19 +4,43 @@
 
 #include "DataLoop.h"
 
+/* Create the default constructor with null pointer and zero count. */
 DataLoop::DataLoop() : start(nullptr), count(0) {}
 
+/* Create the non-default constructor which takes integer input and one element data loop. */
 DataLoop::DataLoop(const int &num) : count(1) {
+    // Create a new Node with null pointers and set them accordingly.
     start = new _Node({num, nullptr, nullptr});
     start->next = start;
     start->prev = start;
 }
 
 DataLoop::DataLoop(const DataLoop & rhs) {
+    if (rhs.count == 0) {
+        return;
+    }
+
+    start = new _Node{rhs.start->data, nullptr, nullptr};
+    start->next = start;
+    start->prev = start;
+
+    _Node *current = rhs.start->next;
+    _Node *newNode, *prevNode = start;
+    while (current != rhs.start) {
+        newNode = new _Node{current->data, prevNode, nullptr};
+        prevNode->next = newNode;
+        current = current->next;
+        prevNode = newNode;
+    }
+
+    prevNode->next = start;
+    start->prev = prevNode;
 
 }
 
+/* Create the destructor which will deallocate dynamically allocatd memory by deletion. */
 DataLoop::~DataLoop() {
+    // First make sure count is greater than zero, then iterate through and deallocate nodes.
     if (count > 0) {
         _Node* currentNode = start;
         _Node* nextNode;
